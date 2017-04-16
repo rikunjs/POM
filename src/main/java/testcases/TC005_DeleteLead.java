45cb782b96ada676b774c37f148e18790643ba4f
Gopinath Jayakumar <gopinath@testleaf.com>
2017-04-16 11:09:18 +0530

    Added Delete TestCase

---
+++ src/main/java/testcases/TC005_DeleteLead.java
@@ -0,0 +1,40 @@
+package testcases;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import pages.LoginPage;
+import wrappers.LeafTapsWrappers;
+
+public class TC005_DeleteLead extends LeafTapsWrappers {
+
+	@BeforeClass
+	public void setValues() {
+		browserName = "chrome";
+		testCaseName = "TC005 - Delete Lead";
+		testDescription = "Verify the user is able to Delete a Lead";
+		category="sanity";
+		authors="Gopi";
+		dataSheetName = "TC005";
+	}
+	
+	@Test(dataProvider="fetchData")
+	public void duplicateLeadPage(String userName, String password) throws InterruptedException
+	{
+		new LoginPage(driver,test)
+		.login(userName, password)
+		.clickCRMSFA()
+		.clickLeads()
+		.clickResultingLead()
+		.Delete();
+		}
+}
+
+
+
+
+
+
+
+
